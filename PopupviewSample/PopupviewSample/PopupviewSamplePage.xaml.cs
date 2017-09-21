bb858d2ffbe555efafd4b6aa6351b4f245e984c5
kiranaluru80 <31380802+kiranaluru80@users.noreply.github.com>
2017-09-21 08:00:46 -0400

    ravipop
    pop view

---
+++ PopupviewSample/PopupviewSample/PopupviewSamplePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using System;
+
+namespace PopupviewSample
+{
+	public partial class PopupviewSamplePage : ContentPage
+	{
+		public PopupviewSamplePage()
+		{
+			InitializeComponent();
+            pickerRef.Clicked += (object sender, EventArgs e) =>
+              {
+					  activityIndicatorLayout.IsVisible = true;
+                OkRef.Clicked+= (object sendravier, EventArgs enemy) =>
+                 {
+                     activityIndicatorLayout.IsVisible = false;
+
+                 };
+                Cancelref.Clicked+=(object senderravi,EventArgs error)=>
+                {
+					  activityIndicatorLayout.IsVisible = false;
+
+				  };
+                      
+              };
+		}
+	}
+}
